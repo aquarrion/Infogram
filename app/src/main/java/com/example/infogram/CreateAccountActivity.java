c4a178d621baf70edbf817cdbb0d44d65b98745f
unknown <zambranafabrizio@gmail.com>
2021-08-26 16:13:12 -0400

    Toolbar

---
+++ app/src/main/java/com/example/infogram/CreateAccountActivity.java
@@ -0,0 +1,26 @@
+package com.example.infogram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        showToolBar(getString(R.string.title_crear_cuenta), true);
+    }
+
+    public void showToolBar(String titulo, boolean botonSubir){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(titulo);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
+    }
+
+}
